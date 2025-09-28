8726f9184dfb993e492c0ea15fec8edcbc44ae6e
Raka Pratama <tama@staffinc.co>
2025-09-28 21:29:36 +0700

    fix(C1): fix error installing dependency on netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 import rehypePrettyCode from 'rehype-pretty-code';
 import rehypeSlug from 'rehype-slug';
 
 export default defineConfig({
 	site: 'https://tamabun.dev',
-	integrations: [mdx(), sitemap(), react()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+	integrations: [mdx(), sitemap(), react(), tailwind()],
 	markdown: {
 		rehypePlugins: [
 			rehypeSlug,
